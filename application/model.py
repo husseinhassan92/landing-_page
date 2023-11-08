e20fa9014ec5af5d9c194ba5d5dcb6d451f4caf9
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-11-08 21:45:02 +0200

    update

--- application/model.py
+++ application/model.py
@@ -11,6 +11,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 from pmdarima.arima import auto_arima
 from statsmodels.tsa.arima.model import ARIMA
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 import warnings
 warnings.filterwarnings("ignore")
 import numpy as np
@@ -83,5 +86,70 @@ class ArimaModelBuilder:
             self.y_pred_wfv = self.y_pred_wfv._append(self.next_pred)
             self.history = self.history._append(self.y_test[self.next_pred.index])
         self.df_predictions = pd.DataFrame({"train" : self.y_train, "y_test" : self.y_test,"y_pred" : self.y_pred_wfv})
+        fig = px.line(self.df_predictions)
+        return fig
+    
+class LSTMModelBuilder:
+    """"""
+    def __init__(self,ticker="", n_observations=10000):
+        self.ticker = ticker
+        self.n_observations = n_observations
+
+    def get_data(self, ticker="", n_observations=10000):
+        df = av.get_daily(ticker)
+        if self.n_observations <= len(df):
+            self.new_df = df.iloc[:n_observations, :]
+        else:
+            self.new_df = df
+        self.new_df = self.new_df.asfreq('d')
+
+    def prepare_data(self):
+        """define the parameters of the model"""
+        self.data= self.new_df[["close"]].fillna(method = "ffill")
+        self.dataset = self.data.values
+        self.scaler = MinMaxScaler(feature_range=(0,1))
+        self.scaled_data = self.scaler.fit_transform(self.dataset)
+        y = self.scaled_data
+        return y
+
+    def split_data(self,train_size=0.95):
+        """split the data to train test sets"""
+        self.training_data_len = int(np.ceil( len(self.dataset) * train_size ))
+        self.train_data = self.scaled_data[0:int(self.training_data_len), :]
+        self.x_train = []
+        self.y_train = []
+        for i in range(60, len(self.train_data)):
+            self.x_train.append(self.train_data[i-60:i, 0])
+            self.y_train.append(self.train_data[i, 0])
+
+        self.x_train = np.array(self.x_train)
+        self.y_train = np.array(self.y_train)
+        self.x_train = np.reshape(self.x_train, (self.x_train.shape[0], self.x_train.shape[1], 1))
+
+        self.test_data = self.scaled_data[self.training_data_len - 60: , :]
+        self.x_test = []
+        self.y_test = self.dataset[self.training_data_len:, :]
+        for i in range(60, len(self.test_data)):
+            self.x_test.append(self.test_data[i-60:i, 0])
+        self.x_test = np.array(self.x_test)
+        self.x_test = np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[1], 1 ))
+
+
+    def graph(self):
+        """"""
+        self.model = Sequential()
+        self.model.add(LSTM(128, return_sequences=True, input_shape= (self.x_train.shape[1], 1)))
+        self.model.add(LSTM(64, return_sequences=False))
+        self.model.add(Dense(25))
+        self.model.add(Dense(1))
+        self.model.compile(optimizer='adam', loss='mean_squared_error')
+        self.model.fit(self.x_train, self.y_train, batch_size=1, epochs=1)
+        self.predictions = self.model.predict(self.x_test)
+        self.predictions = self.scaler.inverse_transform(self.predictions)
+        self.train = self.data[:self.training_data_len]
+        self.valid = self.data[self.training_data_len:]
+        self.valid['Predictions'] = self.predictions
+        self.df_predictions = pd.DataFrame({"train" : self.train['close'],"test" : self.valid['close'],
+                                                "pred" : self.valid['Predictions']})
         fig = px.line(self.df_predictions)
         return fig
